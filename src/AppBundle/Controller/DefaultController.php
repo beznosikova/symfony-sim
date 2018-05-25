e6d4eb221954f69a50a208f8a10a3f30ec96530c
beznosikova <tanya.beznosikova@avivi.com.ua>
2018-05-25 16:04:15 +0300

    add feedback form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\FeedbackType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -13,10 +16,33 @@ class DefaultController extends Controller
      * @Route("/", name="homepage")
      * @Template()
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         $name = "Tanya";
         // return $this->render('@App/default/index.html.twig', compact('name'));
         return compact('name');
+    }
+
+    /**
+     * @Route("/feedback/", name="feedback")
+     */
+    public function feedBackAction(Request $request)
+    {
+        $form = $this->createForm(FeedbackType::class);
+        $form->add('submit', SubmitType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $feedback = $form->getData();
+
+            $em = $this->getDoctrine()->getManager(); // saving entity
+            $em->persist($feedback);
+            $em->flush();
+
+            $this->addFlash('success', 'Saved');
+            return $this->redirectToRoute('feedback');
+        }
+
+         return $this->render('@App/default/feed_back.html.twig', ['feedback_form' => $form->createView()]);
     }
 }
---
+++ src/AppBundle/Form/FeedbackType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FeedbackType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email', EmailType::class)
+            ->add('message')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Feedback'
+//            'data_class' => null // will be array
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_feedback';
+    }
+}
